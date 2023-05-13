08f968c169df538f1ad39e3e8878573e550407d1
ZhangSheng <yanglee2421@gmail.com>
2023-05-13 16:16:52 +0800

    router & serve page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  sassOptions: {
+    additionalData: `@use "@yanglee2421/scss/src" as *;`,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
